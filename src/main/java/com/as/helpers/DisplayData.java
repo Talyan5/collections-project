1e4af27c54ce2f6a1ef1976671ab5353ce73fec2
Anatoliy <anatoliy.starinskiy@gmail.com>
2016-08-27 15:57:18 -0700

    Create Display Helper Data Type

---
+++ src/main/java/com/as/helpers/DisplayData.java
@@ -0,0 +1,28 @@
+/**
+ * File Name: DisplayData.java<br>
+ * Starisnkiy, Anatoliy<br>
+ * Java Boot Camp Exercise<br>
+ * Instructor: Jean-francois Nepton<br>
+ * Created: Aug 27, 2016
+ */
+
+package com.as.helpers;
+
+
+public class DisplayData {
+
+	// Convert the data and return the converted array with new type
+	public static Object[][] convertIntArray(int[][] data) {
+		// TODO return Object [] [] based on int [] [] being
+		System.out.print(data[0][0]);
+		return null;
+	}
+
+	// Display the data passed to the method to the console
+	public static void objectArray(Object[][] data) {
+		// TODO Use System out Calls to display each element
+		// Object [] [] Array being passed in
+	}
+
+
+}
